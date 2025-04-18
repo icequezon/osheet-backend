69b599d39120312164a8ede2804117fc30072c4a
Francis Sebastian Quezon <icequezon@outlook.com>
2025-04-18 15:04:41 +1000

    Create additional config file and setup for env variables

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from blueprints.floor import app as FloorBlueprint
 from blueprints.linen_type import app as LinenTypeBlueprint
 from database import db
 from middleware import check_api_key
+from config import DEBUG
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -20,4 +21,4 @@ app.register_blueprint(FloorBlueprint)
 app.register_blueprint(LinenTypeBlueprint)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=DEBUG)
--- src/middleware.py
+++ src/middleware.py
@@ -1,9 +1,9 @@
-import os
 from flask import request, jsonify
+from .config import API_KEY
+
 
 def check_api_key():
     sent_api_key = request.headers.get('X-Api-Key')
-    API_KEY = os.getenv('API_KEY')
 
-    if sent_api_key or sent_api_key != API_KEY:
-        return jsonify({"error": "Unautorized"}), 401
+    if not sent_api_key or sent_api_key != API_KEY:
+        return jsonify({"error": "Unauthorized"}), 401
